19828591d4650ae24096ae797054782663cafe62
turing_project1 <songchangxia@cann.team>
2025-11-18 19:55:05 +0800

    add softmax_cross_entropy_with_logits
    Co-authored-by: liushiyu0214<liushiyu25@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !301 merge master into master
    
    add softmax_cross_entropy_with_logits
    
    Created-by: LSYlsy0214
    Commit-by: liushiyu0214
    Merged-by: turing_project1
    Description: ## 描述
    新增 softmax_cross_entropy_with_logits 接口
    
    ## 关联的Issue
    [[Bug-Report|缺陷反馈]: 缺少softmax_cross_entropy_with_logits接口](https://gitcode.com/cann/ops-nn/issues/168)
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!301

---
+++ activation/softmax_cross_entropy_with_logits/op_host/op_api/softmax_cross_entropy_with_logits.cpp
@@ -0,0 +1,47 @@
+/**
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file celu_v2.cpp
+ * \brief
+ */
+
+#include "softmax_cross_entropy_with_logits.h"
+#include "opdev/op_dfx.h"
+#include "opdev/shape_utils.h"
+#include "opdev/make_op_executor.h"
+#include "aclnn_kernels/common/op_error_check.h"
+
+using namespace op;
+
+namespace l0op {
+
+OP_TYPE_REGISTER(SoftmaxCrossEntropyWithLogits);
+
+std::tuple<aclTensor*, aclTensor*> SoftmaxCrossEntropyWithLogits(const aclTensor *features, const aclTensor *labels, aclOpExecutor *executor) {
+  L0_DFX(SoftmaxCrossEntropyWithLogits, features, labels);
+  // 获取 features 的 shape
+  const auto& featShape = features->GetViewShape();
+  size_t dimNum = featShape.GetDimNum();
+  op::Shape lossShape({featShape.GetDim(0)});
+  // 分配输出 Tensor
+  auto loss = executor->AllocTensor(lossShape, features->GetDataType());
+  auto backprop = executor->AllocTensor(featShape, features->GetDataType());  // backprop 仍保持原 shape
+  if (loss == nullptr || backprop == nullptr) {
+    OP_LOGE(ACLNN_ERR_INNER_NULLPTR, "alloc loss or backprop tensor failed.");
+    return std::tuple<aclTensor*, aclTensor*>(nullptr, nullptr);
+  }
+  // 注册 AICORE 执行任务
+  ADD_TO_LAUNCHER_LIST_AICORE(
+    SoftmaxCrossEntropyWithLogits, OP_INPUT(features, labels), OP_OUTPUT(loss, backprop));
+  return std::tuple<aclTensor*, aclTensor*>(loss, backprop);
+}
+
+}  // namespace l0op
