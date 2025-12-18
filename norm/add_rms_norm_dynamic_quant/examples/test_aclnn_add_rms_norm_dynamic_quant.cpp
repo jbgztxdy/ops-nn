d9b4dbacc3103559ded6eac5071f1cdf7973da05
liujie12345678 <liujie81@huawei.com>
2025-12-18 14:03:49 +0800

    修改样例代码和md文件一致性
    Co-authored-by: liujie12345678<liujie81@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !430 merge master into master
    
    修改样例代码和md文件一致性
    
    Created-by: liujie12345678
    Commit-by: liujie12345678
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!430

--- norm/add_rms_norm_dynamic_quant/examples/test_aclnn_add_rms_norm_dynamic_quant.cpp
+++ norm/add_rms_norm_dynamic_quant/examples/test_aclnn_add_rms_norm_dynamic_quant.cpp
@@ -76,7 +76,7 @@ int CreateAclTensor(
 
 int main()
 {
-    // 1. （固定写法）device/stream初始化, 参考acl对外接口列表
+    // 1. （固定写法）device/stream初始化，参考acl API手册
     // 根据自己的实际device填写deviceId
     int32_t deviceId = 0;
     aclrtStream stream;
