91186e805974f7912fd895505f1774be2ac43875
chenyanbin8 <chenyanbin8@huawei.com>
2026-01-13 11:40:40 +0800

    修正MaxPoolV3精度问题和opapi告警
    Co-authored-by: chenyanbin8<chenyanbin8@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !658 merge fix/opapi_warning into master
    
    修正MaxPoolV3精度问题和opapi告警
    
    Created-by: chenyanbin8
    Commit-by: chenyanbin8
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    1、消除MaxPoolV3 opapi告警两个
    2、新增max_pool3d_with_argmax_v2算子切换路径，对ksize>1000可能存在的精度问题进行处理
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    泛化了1400个用例PASS
    ![image.png](https://raw.gitcode.com/user-images/assets/7665709/59649735-221a-4676-8f8a-9a78a2747a98/image.png 'image.png')
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!658

--- pooling/max_pool_v3/op_api/aclnn_max_pool.cpp
+++ pooling/max_pool_v3/op_api/aclnn_max_pool.cpp
@@ -10,6 +10,7 @@
 
 #include "aclnn_max_pool.h"
 #include "max_pool_v3.h"
+#include "pooling/max_pool3d_with_argmax_v2/op_host/op_api/max_pool3d_with_argmax_v2.h"
 #include "aclnn_kernels/contiguous.h"
 #include "level0/unsqueeze.h"
 #include "level0/squeeze.h"
@@ -34,11 +35,13 @@ static const int DIMENTION_3 = 3;
 static const int DIMENTION_4 = 4;
 static const int SIZE_1 = 1;
 static const int SIZE_2 = 2;
+static const int SIZE_3 = 3;
 static const int SIZE_4 = 4;
 static const int INDEX_1 = 1;
 static const int INDEX_2 = 2;
 static const int INDEX_3 = 3;
 static const int DILATIONS_HW_DEFAULT = 1;
+static const int64_t MAXPOOL3D_KSIZE_LIMIT = 1000;
 
 // 1980仅支持float16
 static const std::initializer_list<op::DataType> SELF_OUT_DTYPE_SUPPORT_910_LIST = {op::DataType::DT_FLOAT16};
@@ -47,7 +50,7 @@ static const std::initializer_list<op::DataType> SELF_OUT_DTYPE_SUPPORT_910B_LIS
                                                                                      op::DataType::DT_FLOAT};
 static const std::initializer_list<op::DataType> SELF_OUT_DTYPE_SUPPORT_910D_LIST = {
   op::DataType::DT_FLOAT16, op::DataType::DT_FLOAT, op::DataType::DT_BF16, op::DataType::DT_INT32, op::DataType::DT_INT64,
-  op::DataType::DT_UINT8, op::DataType::DT_INT16, op::DataType::DT_INT8, op::DataType::DT_UINT16};                                                                                     
+  op::DataType::DT_UINT8, op::DataType::DT_INT16, op::DataType::DT_INT8, op::DataType::DT_UINT16};
 
 static bool CheckNotNullPtr(const aclTensor* self, const aclIntArray* kernelShape, const aclIntArray* strides,
                             const aclIntArray* pads, const aclIntArray* dilations, aclTensor* out) {
@@ -149,7 +152,7 @@ static inline int64_t divRtn(const int64_t x, const int64_t y) {
 }
 
 // 计算经过MaxPool后的shape的h和w（n,c与input一致，不用计算）
-static inline const int64_t PoolingOutShape(const int64_t inputSize, const int64_t kernelSize, const int64_t padL,
+static inline int64_t PoolingOutShape(const int64_t inputSize, const int64_t kernelSize, const int64_t padL,
                                             const int64_t padR, const int64_t stride, const int64_t dilation,
                                             const bool ceilMode) {
   int64_t outputSize =
@@ -272,7 +275,7 @@ static aclnnStatus CheckParamsLogic(const aclIntArray* kernelShape, const aclInt
   return ACLNN_SUCCESS;
 }
 
-static const bool CheckOutputShape(const aclTensor* self, const int64_t kH, const int64_t kW, const int64_t padHL,
+static bool CheckOutputShape(const aclTensor* self, const int64_t kH, const int64_t kW, const int64_t padHL,
                                    const int64_t padHR, const int64_t padWL, const int64_t padWR, const int64_t dH,
                                    const int64_t dW, const int64_t dilationHW, const int64_t ceilMode, aclTensor* out) {
   const bool is3d = self->GetViewShape().GetDimNum() == DIMENTION_3;
@@ -424,6 +427,187 @@ static const aclTensor* OuttoNDProcess(const aclTensor* output, aclOpExecutor* e
   return outTrans;
 }
 
+static const aclTensor* ExecMaxPoolV3(const aclTensor* self, const aclTensor* selfContiguous, const aclIntArray* kernelShape,
+                                         const aclIntArray* strides, const aclIntArray* pads, const int64_t ceilMode, aclTensor* out, aclOpExecutor* executor) {
+
+  // 如果self是3D，需要扩到4D再调用MaxPoolV3接口
+  const bool isSelf3D = self->GetViewShape().GetDimNum() == DIMENTION_3;
+  auto selfUnsqueezed = isSelf3D ? View3Das4D(selfContiguous, executor) : selfContiguous;
+  CHECK_RET(selfUnsqueezed != nullptr, nullptr);
+
+  const aclTensor* selfProcsResult = selfUnsqueezed;
+  bool isDavid = GetCurrentPlatformInfo().GetSocVersion() == SocVersion::ASCEND910_95;
+  if (!isDavid) {
+    // ND格式-> 5HD格式 NHWC/ NCHW
+    selfProcsResult = Selfto5HDProcess(selfUnsqueezed, executor);
+    CHECK_RET(selfProcsResult != nullptr, nullptr);
+  }
+
+  // 调用MaxPoolV3算子，得到计算结果
+  op::Format storageFormat = selfUnsqueezed->GetStorageFormat();
+  std::string selfUnsqueFormat = op::ToString(storageFormat).GetString();
+
+  auto kernelShape4 = kernelShapeOps(kernelShape, executor);
+  auto strides4 = stridesOps(strides, executor);
+  auto pads4 = padsOps(pads, executor);
+
+  // ND 格式以NCHW处理
+  const bool isSelf4D = selfProcsResult->GetViewShape().GetDimNum() == DIMENTION_4;
+  if (isSelf4D && selfProcsResult->GetStorageFormat() == op::Format::FORMAT_ND) {
+    selfUnsqueFormat = "NCHW";
+  }
+  auto maxPoolOutResult =
+      l0op::MaxPoolV3(selfProcsResult, kernelShape4, strides4, pads4, selfUnsqueFormat, ceilMode, executor);
+  CHECK_RET(maxPoolOutResult != nullptr, nullptr);
+
+  const aclTensor* outProcsResult = maxPoolOutResult;
+  if (!isDavid) {
+    // 对返回结果进行处理，进行5HD格式转换
+    outProcsResult = OuttoNDProcess(maxPoolOutResult, executor);
+    CHECK_RET(outProcsResult != nullptr, nullptr);
+  }
+
+  const op::Format& dstFormat = out->GetStorageFormat();
+  auto outSqueezed = isSelf3D ? View4Das3D(outProcsResult, dstFormat, executor) : outProcsResult;
+  CHECK_RET(outSqueezed != nullptr, nullptr);
+
+  return outSqueezed;
+}
+
+static inline const aclTensor* View3Das5D(const aclTensor* input, aclOpExecutor* executor)
+{
+    // CHW -> unsqueeze -> reformat -> 1C1HW(NCDHW, N=1, D=1)
+    // unsqueeze input into 5D
+    FVector<int64_t> dimData{0, 2}; // Unsqueeze at dimension 0, 2
+    const aclIntArray* dim2 = executor->AllocIntArray(dimData.data(), 2);
+    auto unsqueezedInput = l0op::UnsqueezeNd(input, dim2, executor);
+    CHECK_RET(unsqueezedInput != nullptr, nullptr);
+    // reformat to NCDHW
+    auto reformatInput = l0op::ReFormat(unsqueezedInput, op::Format::FORMAT_NCDHW, executor);
+    CHECK_RET(reformatInput != nullptr, nullptr);
+
+    return reformatInput;
+}
+
+static inline const aclTensor* View4Das5D(const aclTensor* input, aclOpExecutor* executor)
+{
+    // NCHW -> unsqueeze -> reformat -> NC1HW(NCDHW, D=1)
+    // unsqueeze input into 5D
+    const int64_t dim = 2; // Unsqueeze at dimension 2
+    auto unsqueezedInput = l0op::UnsqueezeNd(input, dim, executor);
+    CHECK_RET(unsqueezedInput != nullptr, nullptr);
+    // reformat to NCDHW
+    auto reformatInput = l0op::ReFormat(unsqueezedInput, op::Format::FORMAT_NCDHW, executor);
+    CHECK_RET(reformatInput != nullptr, nullptr);
+
+    return reformatInput;
+}
+
+static inline const aclTensor* View5Das4D(const aclTensor* input, const op::Format& format, aclOpExecutor* executor)
+{
+    // NC1HW(NCDHW, D=1) -> squeeze -> reformat -> NCHW
+    // squeeze out into 4D
+    const int64_t dim = 2; // Squeeze out dimension 2
+    auto squeezedInput = l0op::SqueezeNd(input, dim, executor);
+    CHECK_RET(squeezedInput != nullptr, nullptr);
+    // reformat to NCHW
+    auto reformatInput = l0op::ReFormat(squeezedInput, format, executor);
+    CHECK_RET(reformatInput != nullptr, nullptr);
+
+    return reformatInput;
+}
+
+static inline const aclTensor* View5Das3D(const aclTensor* input, const op::Format& format, aclOpExecutor* executor)
+{
+    // 1C1HW(NCDHW, N=1, D=1) -> squeeze -> reformat -> CHW
+    // squeeze out into 3D
+    FVector<int64_t> dimData{0, 2}; // Squeeze out dimensions 0 and 2
+    const aclIntArray* dim2 = executor->AllocIntArray(dimData.data(), 2);
+    auto squeezedInput = l0op::SqueezeNd(input, dim2, executor);
+    CHECK_RET(squeezedInput != nullptr, nullptr);
+    // reformat to NCHW
+    auto reformatInput = l0op::ReFormat(squeezedInput, format, executor);
+    CHECK_RET(reformatInput != nullptr, nullptr);
+
+    return reformatInput;
+}
+
+static std::tuple<aclIntArray*, aclIntArray*, aclIntArray*, aclIntArray*> ProcessAttrTo3DParams(
+    const aclIntArray* kernelSize, const aclIntArray* stride, const aclIntArray* padding, aclOpExecutor* executor)
+{
+    // 处理 kernel 尺寸
+    const aclIntArray& kernelRef = *kernelSize;
+    const int64_t kernelH = kernelRef[0];
+    const int64_t kernelW = (kernelRef.Size() == 1) ? kernelH : kernelRef[INDEX_1];
+
+    // 处理步长
+    const aclIntArray& strideRef = *stride;
+    const int64_t strideH = (strideRef.Size() == 0) ? 1 : strideRef[0];
+    const int64_t strideW = (strideRef.Size() == 0) ? 1 : ((strideRef.Size() == 1) ? strideH : strideRef[INDEX_1]);
+
+    // 处理填充
+    const aclIntArray& paddingRef = *padding;
+    const int64_t paddingH = (paddingRef.Size() == 0) ? 0 : paddingRef[0];
+    const int64_t paddingW = (paddingRef.Size() == 0) ? 0 : ((paddingRef.Size() == 1) ? paddingH : paddingRef[INDEX_1]);
+
+    // 构建三维参数数组
+    FVector<int64_t> kernelSizeData{1, kernelH, kernelW};
+    FVector<int64_t> strideSizeData{1, strideH, strideW};
+    FVector<int64_t> paddingSizeData{0, paddingH, paddingW};
+    FVector<int64_t> dilationSizeData{1, 1, 1};
+
+    // 分配内存并返回
+    aclIntArray* kernelSize3D = executor->AllocIntArray(kernelSizeData.data(), SIZE_3);
+    aclIntArray* stride3D = executor->AllocIntArray(strideSizeData.data(), SIZE_3);
+    aclIntArray* padding3D = executor->AllocIntArray(paddingSizeData.data(), SIZE_3);
+    aclIntArray* dilation3D = executor->AllocIntArray(dilationSizeData.data(), SIZE_3);
+    return {kernelSize3D, stride3D, padding3D, dilation3D};
+}
+
+static inline bool ExecMaxPool3DCapable(const aclIntArray* kernelShape, const aclIntArray* pads) {
+  bool padLengthFlag = true;
+  const aclIntArray& padsRef = *pads;
+  // pads-4D:[H_top、W_left、H_bottom、W_right]
+  if (pads->Size() == SIZE_4 && ((padsRef[0] != padsRef[INDEX_2]) || (padsRef[INDEX_1] != padsRef[INDEX_3]))) {
+      padLengthFlag = false;
+  }
+
+  bool supportMaxPool3DWithArgmaxV2 = GetCurrentPlatformInfo().GetSocVersion() == SocVersion::ASCEND910B ||
+                                      GetCurrentPlatformInfo().GetSocVersion() == SocVersion::ASCEND910_93;
+
+  const aclIntArray& kernelRef = *kernelShape;
+  const int64_t kH = kernelRef[0];
+  const int64_t kW = (kernelRef.Size() == SIZE_1) ? kH : kernelRef[INDEX_1];
+  bool kLengthFlag = kH > MAXPOOL3D_KSIZE_LIMIT || kW > MAXPOOL3D_KSIZE_LIMIT;
+
+  return padLengthFlag && supportMaxPool3DWithArgmaxV2 && kLengthFlag;
+}
+
+static const aclTensor* ExecMaxPool3DWithArgmaxV2(const aclTensor* self, const aclTensor* selfContiguous, const aclIntArray* kernelShape,
+                                         const aclIntArray* strides, const aclIntArray* pads, const int64_t ceilMode, aclTensor* out, aclOpExecutor* executor) {
+
+    // 如果self是3d，需要扩到5d，再调用MaxPool3DWithArgmaxV2接口
+    const bool isSelf3D = self->GetViewShape().GetDimNum() == DIMENTION_3;
+    auto selfUnsqueezed = isSelf3D ? View3Das5D(selfContiguous, executor) : View4Das5D(selfContiguous, executor);
+    CHECK_RET(selfUnsqueezed != nullptr, nullptr);
+
+    // 更新2D属性适配3D
+    auto [kernel3D, stride3D, padding3D, dilation3D] = ProcessAttrTo3DParams(kernelShape, strides, pads, executor);
+    CHECK_RET((kernel3D != nullptr) && (stride3D != nullptr) && (padding3D != nullptr) && (dilation3D != nullptr), nullptr);
+    // 调用3D接口
+    std::string dataFormat = "NCDHW";
+    auto [outResult, indicesResult] = l0op::MaxPool3DWithArgmaxV2Ncdhw(
+        selfUnsqueezed, kernel3D, stride3D, padding3D, dilation3D, ceilMode, dataFormat, executor);
+    CHECK_RET(outResult != nullptr, nullptr);
+    CHECK_RET(indicesResult != nullptr, nullptr);
+    const op::Format& outFormat = out->GetStorageFormat();
+    auto outResultSqueezed = isSelf3D ? View5Das3D(outResult, outFormat, executor) :
+                                        View5Das4D(outResult, outFormat, executor);
+    CHECK_RET(outResultSqueezed != nullptr, nullptr);
+
+    return outResultSqueezed;
+}
+
 aclnnStatus aclnnMaxPoolGetWorkspaceSize(const aclTensor* self, const aclIntArray* kernelShape,
                                          const aclIntArray* strides, const int64_t autoPad, const aclIntArray* pads,
                                          const aclIntArray* dilations, const int64_t ceilMode, aclTensor* out,
@@ -450,45 +634,12 @@ aclnnStatus aclnnMaxPoolGetWorkspaceSize(const aclTensor* self, const aclIntArra
   auto selfContiguous = l0op::Contiguous(self, uniqueExecutor.get());
   CHECK_RET(selfContiguous != nullptr, ACLNN_ERR_INNER_NULLPTR);
 
-  // 如果self是3D，需要扩到4D再调用MaxPoolV3接口
-  const bool isSelf3D = self->GetViewShape().GetDimNum() == DIMENTION_3;
-  auto selfUnsqueezed = isSelf3D ? View3Das4D(selfContiguous, uniqueExecutor.get()) : selfContiguous;
-  CHECK_RET(selfUnsqueezed != nullptr, ACLNN_ERR_INNER_NULLPTR);
-
-  const aclTensor* selfProcsResult = selfUnsqueezed;
-  bool isDavid = GetCurrentPlatformInfo().GetSocVersion() == SocVersion::ASCEND910_95;
-  if (!isDavid) {
-    // ND格式-> 5HD格式 NHWC/ NCHW
-    selfProcsResult = Selfto5HDProcess(selfUnsqueezed, uniqueExecutor.get());
-    CHECK_RET(selfProcsResult != nullptr, ACLNN_ERR_INNER_NULLPTR);
+  const aclTensor* outSqueezed = nullptr;
+  if (ExecMaxPool3DCapable(kernelShape, pads)){
+    outSqueezed = ExecMaxPool3DWithArgmaxV2(self, selfContiguous, kernelShape, strides, pads, ceilMode, out, uniqueExecutor.get());
+  } else{
+    outSqueezed = ExecMaxPoolV3(self, selfContiguous, kernelShape, strides, pads, ceilMode, out, uniqueExecutor.get());
   }
-
-  // 调用MaxPoolV3算子，得到计算结果
-  op::Format storageFormat = selfUnsqueezed->GetStorageFormat();
-  std::string selfUnsqueFormat = op::ToString(storageFormat).GetString();
-
-  auto kernelShape4 = kernelShapeOps(kernelShape, uniqueExecutor.get());
-  auto strides4 = stridesOps(strides, uniqueExecutor.get());
-  auto pads4 = padsOps(pads, uniqueExecutor.get());
-
-  // ND 格式以NCHW处理
-  const bool isSelf4D = selfProcsResult->GetViewShape().GetDimNum() == DIMENTION_4;
-  if (isSelf4D && selfProcsResult->GetStorageFormat() == op::Format::FORMAT_ND) {
-    selfUnsqueFormat = "NCHW";
-  }
-  auto maxPoolOutResult =
-      l0op::MaxPoolV3(selfProcsResult, kernelShape4, strides4, pads4, selfUnsqueFormat, ceilMode, uniqueExecutor.get());
-  CHECK_RET(maxPoolOutResult != nullptr, ACLNN_ERR_INNER_NULLPTR);
-
-  const aclTensor* outProcsResult = maxPoolOutResult;
-  if (!isDavid) {
-    // 对返回结果进行处理，进行5HD格式转换
-    outProcsResult = OuttoNDProcess(maxPoolOutResult, uniqueExecutor.get());
-    CHECK_RET(outProcsResult != nullptr, ACLNN_ERR_INNER_NULLPTR);
-  }
-
-  const op::Format& dstFormat = out->GetStorageFormat();
-  auto outSqueezed = isSelf3D ? View4Das3D(outProcsResult, dstFormat, uniqueExecutor.get()) : outProcsResult;
   CHECK_RET(outSqueezed != nullptr, ACLNN_ERR_INNER_NULLPTR);
 
   // 如果出参out是非连续Tensor，需要把计算完的连续Tensor转非连续
