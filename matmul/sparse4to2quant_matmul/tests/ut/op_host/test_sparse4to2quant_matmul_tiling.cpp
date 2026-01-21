3daf188028158f36e9d92f96c7071552ee62268e
wangzitao_leo <wangzitao4@huawei.com>
2026-01-21 11:19:31 +0800

    avoid global var in sparse4to2matmul ut
    Co-authored-by: ZitaoWang<wangzitao4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !832 merge nn_sync_dev into master
    
    avoid global var in sparse4to2matmul ut
    
    Created-by: wangzitao_leo
    Commit-by: ZitaoWang
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!832

--- matmul/sparse4to2quant_matmul/tests/ut/op_host/test_sparse4to2quant_matmul_tiling.cpp
+++ matmul/sparse4to2quant_matmul/tests/ut/op_host/test_sparse4to2quant_matmul_tiling.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -415,7 +415,9 @@ static void ThreadFunc(const Sparse4to2QuantMatmulTilingTestParam *params, size_
     }
 }
 
-mutex compileMutex;
+namespace {
+mutex sparseQuantMatmulcompileMutex;
+}
 
 static void ThreadFuncPrepare(const Sparse4to2QuantMatmulTilingTestParam *params, size_t testcaseNum, size_t threadIdx,
                        size_t threadNum, map<size_t, Sparse4to2QuantMatmulCompileInfo> &compileInfos)
@@ -428,7 +430,7 @@ static void ThreadFuncPrepare(const Sparse4to2QuantMatmulTilingTestParam *params
     params[threadIdx].Prepare(compileInfo);
 
     {
-        lock_guard<mutex> lock(compileMutex);
+        lock_guard<mutex> lock(sparseQuantMatmulcompileMutex);
         compileInfos[threadIdx] = compileInfo;
     }
 }
