8d18f113c1f64b997ef4e391405d7c38a4698092
LSYlsy0214 <liushiyu25@h-partners.com>
2026-01-28 17:15:33 +0800

    add logit A5
    Co-authored-by: LSYlsy0214<liushiyu25@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1097 merge master into master
    
    add logit A5
    
    Created-by: LSYlsy0214
    Commit-by: LSYlsy0214
    Merged-by: cann-robot
    Description: ## 描述
    logit算子新增支持A5
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-nn/issues/574
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!1097

--- loss/logit/op_kernel/logit.cpp
+++ loss/logit/op_kernel/logit.cpp
@@ -24,7 +24,7 @@ extern "C" __global__ __aicore__ void logit(GM_ADDR input, GM_ADDR output, GM_AD
 
     GM_ADDR userWs = nullptr;
 
-#if __CCE_AICORE__ == 220 || (defined(__NPU_ARCH__) && __NPU_ARCH__ == 3003)
+#if __CCE_AICORE__ == 220 || __CCE_AICORE__ == 310 || (defined(__NPU_ARCH__) && __NPU_ARCH__ == 3003)
     if (TILING_KEY_IS(1)) {
         LogitND<half> op;
         op.Init(input, output, userWs, &tilingData);
