640538d50a70d86a7fc8385fa4730f6bbd5ae6aa
BruceLP <lipeng192@huawei.com>
2026-01-31 16:45:59 +0800

    update aclnn topk head file
    Co-authored-by: BruceLP<lipeng192@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1165 merge topk_head into master
    
    update aclnn topk head file
    
    Created-by: BruceLP
    Commit-by: BruceLP
    Merged-by: cann-robot
    Description: ## 描述
    更新aclnn_Topk.h中的注释，删除接口字段相关描述
    
    ## 关联的Issue
    关联Issue #674
    
    ## 测试
    不涉及
    
    ## 文档更新
    不涉及
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：更新aclnn_Topk.h头文件注释
    
    
    
    
    See merge request: cann/ops-nn!1165

--- index/gather_elements/op_api/aclnn_topk.h
+++ index/gather_elements/op_api/aclnn_topk.h
@@ -20,30 +20,8 @@ extern "C" {
 
 /**
  * @brief aclnnTopk的第一段接口，根据具体的计算流程，计算workspace大小。
- * @domain aclnn_ops_train
  *
  * 算子功能：完成计算输入的k个极值及下标。
- *
- * @param [in] self: npu
- * npu device侧的aclTensor，数据类型支持INT8、UINT8、INT16、INT32、INT64、FLOAT16、FLOAT32、DOUBLE。
- * 支持连续和非连续的Tensor，数据格式支持ND。
- * @param [in] k:
- * int64_t类型整数。表示计算维度上输出的极值个数。取值范围为[0, self.size(dim)]。
- * @param [in] dim:
- * int64_t类型整数。表示计算维度。取值范围为[-self.dim(), self.dim())。
- * @param [in] largest:
- * bool类型数据。True表示计算维度上的结果应由大到小输出，False表示计算维度上的结果由小到大输出。
- * @param [in] sorted:
- * bool类型数据。True表示输出结果需要排序（若largest为True则结果从大到小排序，若largest为False则结果从小到大排序），
- * False表示输出结果不排序，按输入时的数据顺序输出。
- * @param [in] valuesOut:
- * dnpu device侧的aclTensor，数据类型支持INT8、UINT8、INT16、INT32、INT64、FLOAT16、FLOAT32、DOUBLE，
- * 且数据类型与self保持一致，支持连续和非连续的Tensor，数据格式支持ND。
- * @param [in] indicesOut:
- * npu device侧的aclTensor，数据类型支持INT64。支持连续和非连续的Tensor，数据格式支持ND。
- * @param [out] workspaceSize: 返回用户需要在npu device侧申请的workspace大小。
- * @param [out] executor: 返回op执行器，包含算子计算流程。
- * @return aclnnStatus: 返回状态码。
  */
 ACLNN_API aclnnStatus aclnnTopkGetWorkspaceSize(const aclTensor* self, int64_t k, int64_t dim, bool largest,
                                                 bool sorted, aclTensor* valuesOut, aclTensor* indicesOut,
@@ -52,12 +30,6 @@ ACLNN_API aclnnStatus aclnnTopkGetWorkspaceSize(const aclTensor* self, int64_t k
  * @brief aclnnTopk的第二段接口，用于执行计算。
  *
  * 算子功能：完成计算输入的k个极值及下标。
- *
- * @param [in] workspace: 在npu device侧申请的workspace内存起址。
- * @param [in] workspaceSize: 在npu device侧申请的workspace大小，由第一段接口aclnnTopkGetWorkspaceSize获取。
- * @param [in] stream: acl stream流。
- * @param [in] executor: op执行器，包含了算子计算流程。
- * @return aclnnStatus: 返回状态码。
  */
 ACLNN_API aclnnStatus aclnnTopk(void* workspace, uint64_t workspaceSize, aclOpExecutor* executor,
                                 const aclrtStream stream);
