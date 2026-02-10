9d632468a4a9de1e70b6b3cd71b1c9265143a209
wuyewei <wuyewei@huawei.com>
2026-02-10 11:00:27 +0800

    aclnnMaxPool3dWithArgmax 接口注释修改
    Co-authored-by: w00852310<wuyewei@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1689 merge master into master
    
    aclnnMaxPool3dWithArgmax 接口注释修改
    
    Created-by: wuyewei
    Commit-by: w00852310
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    aclnnMaxPool3dWithArgmax 接口注释修改，改为只描述功能。
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    [aclnnMaxPool3dWithArgmax 接口更新，需更新接口注释](https://gitcode.com/cann/ops-nn/issues/922)
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    仅注释修改
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    更新了aclnn_max_pool3d_with_argmax.h文件注释
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [x] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!1689

--- pooling/max_pool3d_with_argmax_v2/op_api/aclnn_max_pool3d_with_argmax.h
+++ pooling/max_pool3d_with_argmax_v2/op_api/aclnn_max_pool3d_with_argmax.h
@@ -22,21 +22,6 @@ extern "C" {
  * @brief aclnnMaxPool3dWithArgmax First segment interface. Calculate the workspace size based on the specific
  * calculation process. Function description: Calculates the aclnnMaxPool3dWithArgmax from the input tensor, at the
  * output contains 2 tensors: out and indices
- * @domain aclnn_ops_infer
- * @param [in] self: aclTensor on the NPU device. the data type can be float32/float16/bfloat16. The data format
- * supports ND.
- * @param [in] kernelSize: aclIntArray type, indicating the maxpooling window size.
- * @param [in] stride: aclIntArray type, the step size of the window movement.
- * @param [in] padding: aclIntArray type, number of padding layers for each edge. The value is negative infinity.
- * @param [in] dilation: aclIntArray type, controls the stride of elements in the window.
- * @param [in] ceilMode: bool type, when true, the output shape is calculated using round-up method. By default is
- * rounding down.
- * @param [in] out: aclTensor on the NPU device. the data type can be float32/float16/bfloat16. The data format supports
- * ND.
- * @param [in] indices: aclTensor on the NPU device. the data type can be int32. The data format supports ND.
- * @param [out] workspaceSize: Returns the workspace size that the user needs to apply for on the npu device side.
- * @param [out] executor: Return the op executor, including the operator calculation process.
- * @return aclnnStatus: Return the status code.
  */
 
 ACLNN_API aclnnStatus aclnnMaxPool3dWithArgmaxGetWorkspaceSize(
@@ -45,12 +30,6 @@ ACLNN_API aclnnStatus aclnnMaxPool3dWithArgmaxGetWorkspaceSize(
     aclOpExecutor** executor);
 /**
  * @brief A second interface of aclnnMaxPool3dWithArgmax, used to perform calculation.
- * @param [in] workspace: start address of the workspace memory allocated on the NPU device.
- * @param [in] workspaceSize: size of the workspace applied on the NPU device, which is obtained by calling the first
- * segment interface aclnnMaxPool3dWithArgmaxGetWorkspaceSize.
- * @param [in] exector: op executor, including the operator calculation process.
- * @param [in] stream: acl stream.
- * @return aclnnStatus: returned status code
  */
 
 ACLNN_API aclnnStatus
