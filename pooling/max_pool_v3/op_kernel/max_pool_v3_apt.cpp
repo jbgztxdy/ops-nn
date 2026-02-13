99ce73e3ab5a24c2c4d2d907969f94260dd21f11
dong-yanrong <dongyanrong@h-partners.com>
2026-02-13 11:26:10 +0800

    max pool v3 dispose precision fail
    Co-authored-by: dong-yanrong<dongyanrong@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1833 merge pool_v3 into master
    
    max pool v3 dispose precision fail
    
    Created-by: dong-yanrong
    Commit-by: dong-yanrong
    Merged-by: cann-robot
    Description: ## 描述
    场景：Ascend950上aclnnMaxPool接口部分用例在测通路以及精度的场景下，会出现精度对比失败的问题
    原因：aclnnMaxPool接口会调用到max_pool_v3算子，在kernel侧，tilingkey路由错误，导致用例进入错误的模板，导致精度不对
    修改方案：修正max_pool_v3算子在kernel侧，tilingkey路由逻辑
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-nn/issues/1032
    
    ## 测试
    aclnnMaxPool接口，本地白盒验证和网络用例验证测试aclnn通路以及精度，二进制通路精度均通过。
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!1833

--- pooling/max_pool_v3/op_kernel/max_pool_v3_apt.cpp
+++ pooling/max_pool_v3/op_kernel/max_pool_v3_apt.cpp
@@ -13,6 +13,7 @@
  */
 
 #include <cstdint>
+#include "kernel_operator.h"
 #include "arch35/max_pool_v3_big_kernel.h"
 #include "arch35/max_pool_v3_small_kernel.h"
 #include "arch35/max_pool_v3_small_kernel_pad.h"
@@ -31,51 +32,35 @@ extern "C" __global__ __aicore__ void max_pool_v3(GM_ADDR x, GM_ADDR y, GM_ADDR 
 {
     AscendC::TPipe pipeBase;
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_AIV_ONLY);
-    TILING_KEY_IS(BIG_KERNEL_FORMAT_NCHW);
-    TILING_KEY_IS(SMALL_KERNEL_NO_PADDING_FORMAT_NCHW);
-    TILING_KEY_IS(SMALL_KERNEL_PADDING_FORMAT_NCHW);
-    TILING_KEY_IS(BIG_KERNEL_FORMAT_NHWC);
-    TILING_KEY_IS(SMALL_KERNEL_NO_PADDING_FORMAT_NHWC);
-    TILING_KEY_IS(SMALL_KERNEL_PADDING_FORMAT_NHWC);
-    #if TILING_KEY_X == BIG_KERNEL_FORMAT_NCHW
+    if (TILING_KEY_IS(BIG_KERNEL_FORMAT_NCHW)) {
         GET_TILING_DATA_WITH_STRUCT(MaxPoolV3BigKernelTilingData, tilingData, tiling);
         MaxPoolV3::MaxPoolV3BigKernel<DTYPE_X> op(&pipeBase, &tilingData);
         op.Init(x, y);
         op.Process();
-    #endif
-
-    #if TILING_KEY_X == SMALL_KERNEL_NO_PADDING_FORMAT_NCHW
+    } else if (TILING_KEY_IS(SMALL_KERNEL_NO_PADDING_FORMAT_NCHW)) {
         GET_TILING_DATA_WITH_STRUCT(MaxPoolV3SmallKernelTilingData, tilingData, tiling);
         MaxPoolV3::MaxPoolV3SmallKernel<DTYPE_X> op(&pipeBase, &tilingData);
         op.Init(x, y);
         op.Process();
-    #endif
-
-    #if TILING_KEY_X == SMALL_KERNEL_PADDING_FORMAT_NCHW
+    } else if (TILING_KEY_IS(SMALL_KERNEL_PADDING_FORMAT_NCHW)) {
         GET_TILING_DATA_WITH_STRUCT(MaxPoolV3SmallKernelTilingData, tilingData, tiling);
         MaxPoolV3::MaxPoolV3SmallPadKernel<DTYPE_X> op(&pipeBase, &tilingData);
         op.Init(x, y);
         op.Process();
-    #endif
-
-    #if TILING_KEY_X == BIG_KERNEL_FORMAT_NHWC
+    } else if (TILING_KEY_IS(BIG_KERNEL_FORMAT_NHWC)) {
         GET_TILING_DATA_WITH_STRUCT(MaxPoolV3NHWCBigKernelTilingData, tilingData, tiling);
         MaxPoolV3::MaxPoolV3NHWCBigKernel<DTYPE_X> op(&pipeBase, &tilingData);
         op.Init(x, y);
         op.Process();
-    #endif
-
-    #if TILING_KEY_X == SMALL_KERNEL_NO_PADDING_FORMAT_NHWC
+    } else if (TILING_KEY_IS(SMALL_KERNEL_NO_PADDING_FORMAT_NHWC)) {
         GET_TILING_DATA_WITH_STRUCT(MaxPoolV3NHWCSmallKernelTilingData, tilingData, tiling);
         MaxPoolV3::MaxPoolV3NHWCSmallKernel<DTYPE_X> op(&pipeBase, &tilingData);
         op.Init(x, y);
         op.Process();
-    #endif
-
-    #if TILING_KEY_X == SMALL_KERNEL_PADDING_FORMAT_NHWC
+    } else if (TILING_KEY_IS(SMALL_KERNEL_PADDING_FORMAT_NHWC)) {
         GET_TILING_DATA_WITH_STRUCT(MaxPoolV3NHWCSmallKernelTilingData, tilingData, tiling);
         MaxPoolV3::MaxPoolV3NHWCSmallPadKernel<DTYPE_X> op(&pipeBase, &tilingData);
         op.Init(x, y);
         op.Process();
-    #endif
+    }
 }
