faaba317c9a1e64c1f97852fadeee934b6141b72
hw-zhangpanpan <zhangpanpan5@huawei.com>
2026-02-14 09:12:01 +0800

    crossv2适配ascend950
    Co-authored-by: hw-zhangpanpan<zhangpanpan5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1431 merge master into master
    
    crossv2适配ascend950
    
    Created-by: hw-zhangpanpan
    Commit-by: hw-zhangpanpan
    Merged-by: cann-robot
    Description: ## 描述
    crossv2适配ascend950
    
    ## 关联的Issue
    [Requirement|需求建议]: crossv2算子适配ascend950 [#980](https://gitcode.com/cann/ops-nn/issues/980) 
    
    ## 测试
    精度测试、内存测试、确定性测试通过
    
    ## 文档更新
    暂不涉及
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    crossv2适配ascend950
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    
    See merge request: cann/ops-nn!1431

--- loss/cross_v2/op_host/cross_v2_def.cpp
+++ loss/cross_v2/op_host/cross_v2_def.cpp
@@ -56,6 +56,7 @@ public:
         this->Attr("dim").AttrType(OPTIONAL).Int(DEFAULT_DIM);
         this->AICore().AddConfig("ascend910b");
         this->AICore().AddConfig("ascend910_93");
+        this->AICore().AddConfig("ascend950");
     }
 };
 
