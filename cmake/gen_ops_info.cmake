4b5fcbf98072167ed5ba2b775da554da5086bbbb
cong-jiyu <congjiyu@h-partners.com>
2026-02-26 21:55:22 +0800

    fix: 同一台机器同时编译kernel导致卡死
    Co-authored-by: 丛吉钰<congjiyu@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2082 merge kernel into master
    
    fix: 同一台机器同时编译kernel导致卡死
    
    Created-by: cong-jiyu
    Commit-by: 丛吉钰
    Merged-by: cann-robot
    Description: ## 描述
    回滚cmake的静默打印
    
    ## 关联的Issue
    [#1158](https://gitcode.com/cann/ops-nn/issues/1158) 
    
    ## 测试
    同一台机器多人编译
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!2082

--- cmake/gen_ops_info.cmake
+++ cmake/gen_ops_info.cmake
@@ -416,7 +416,7 @@ function(compile_from_config)
     List(APPEND _BUILD_COMMAND export BIN_FILENAME_HASHED=1 &&)
     List(APPEND _BUILD_COMMAND export ASCEND_SLOG_PRINT_TO_STDOUT=1 &&)
     List(APPEND _BUILD_COMMAND ${_ASCENDC_ENV_VAR} stdbuf -oL bash ${OPS_KERNEL_BINARY_SCRIPT}/build_binary_op_exe_task.sh ${CONFCMP_OUT_DIR}/bin ${idx})
-    List(APPEND _BUILD_COMMAND && echo $(MAKE) &> /dev/null)
+    List(APPEND _BUILD_COMMAND && echo $(MAKE))
     add_custom_target(exe_compile_${CONFCMP_COMPUTE_UNIT}_${idx}
       COMMAND ${_BUILD_COMMAND}
       WORKING_DIRECTORY ${OPS_KERNEL_BINARY_SCRIPT}
