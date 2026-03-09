13ba0822e2e63eaa1122745843eb473d26980de1
Framework111 <wangyucai2@huawei.com>
2026-03-09 10:01:51 +0800

    add Renrom in A5
    Co-authored-by: Framework111<wangyucai2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2375 merge master into master
    
    add Renrom in A5
    
    Created-by: Framework111
    Commit-by: Framework111
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。--> 新增Renorm A5实现
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000--> 关联issue #1357
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。--> 
    ![image.png](https://raw.gitcode.com/user-images/assets/7665709/25a4d44a-809b-49ad-b3f6-589db3fc8a15/image.png 'image.png')
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。--> 无
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!2375

--- norm/renorm/op_api/aclnn_renorm.cpp
+++ norm/renorm/op_api/aclnn_renorm.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -92,7 +92,7 @@ static inline bool CheckRenormShape(const aclTensor* self, const aclTensor* out)
 static inline bool IsSupportPValue(const aclScalar* p)
 {
     float x = p->ToFloat();
-    if (x < 0) {
+    if (x <= 0) {
         return false;
     }
     return true;
