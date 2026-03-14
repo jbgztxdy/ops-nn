8b630b748bd582ddd68a43714129b72114d4b210
lvmengge <lvmengge@huawei.com>
2026-03-14 11:48:15 +0800

    fix 使用GetSortMaxMinTmpSize获取排序需要的临时buffer大小
    Co-authored-by: 吕梦鸽<lvmengge@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2686 merge master into master
    
    fix 使用GetSortMaxMinTmpSize获取排序需要的临时buffer大小
    
    Created-by: lvmengge
    Commit-by: 吕梦鸽
    Merged-by: cann-robot
    Description: ## 描述
    使用GetSortMaxMinTmpSize获取排序需要的临时buffer大小
    
    ## 关联的Issue
    关联Issue [#1491](https://gitcode.com/cann/ops-nn/issues/1491) 
    ## 测试
    网络验证和本地st验证
    
    ## 文档更新
    不涉及
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [√] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!2686

--- index/scatter_elements_v2/op_host/scatter_elements_v2_asc_tiling.cpp
+++ index/scatter_elements_v2/op_host/scatter_elements_v2_asc_tiling.cpp
@@ -338,9 +338,19 @@ void ScatterElementsV2AscTiling::GetCastTypeSize()
     }
 }
 
-uint32_t ScatterElementsV2AscTiling::GetMaxSortTmpBuf([[maybe_unused]] int64_t sortDim)
+uint32_t ScatterElementsV2AscTiling::GetMaxSortTmpBuf(int64_t sortDim)
 {
-    return MAX_SORT_SPACE;
+    std::vector<int64_t> shapeVec = {sortDim};
+    ge::Shape srcShape(shapeVec);
+    AscendC::SortConfig config;
+    config.type = AscendC::SortType::RADIX_SORT;
+    config.isDescend = false;
+    config.hasSrcIndex = false;
+    config.hasDstIndex = true;
+    uint32_t maxValue = 0;
+    uint32_t minValue = 0;
+    GetSortMaxMinTmpSize(srcShape, indicesDtype_, ge::DT_UINT32, false, config, maxValue, minValue);
+    return maxValue;
 }
 
 /**
