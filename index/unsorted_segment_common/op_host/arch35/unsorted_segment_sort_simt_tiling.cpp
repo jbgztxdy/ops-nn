0078664c5450403e2b268f2efa20695ef6d084bb
cann-robot <songchangxia@cann.team>
2026-03-17 14:26:03 +0800

    UnsortedSegmentMin排序准入条件修改
    Co-authored-by: 何金鹏<hejinpeng5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2733 merge usmdts into master
    
    UnsortedSegmentMin排序准入条件修改
    
    Created-by: jinpenghe
    Commit-by: 何金鹏
    Merged-by: cann-robot
    Description: ## 描述
    unsortedsegmentMin simt排序准入条件修改
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-nn/issues/1503
    
    ## 测试
    [[1, 2, 8, 7, 94, 3, 9, 1], [1, 2, 8, 7, 94]] {'num_segments':519} 
    [[16, 15, 19, 20, 1], [16, 15, 19]] {'num_segments':120
    用例进入正确的分支，性能提升
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!2733

--- index/unsorted_segment_common/op_host/arch35/unsorted_segment_sort_simt_tiling.cpp
+++ index/unsorted_segment_common/op_host/arch35/unsorted_segment_sort_simt_tiling.cpp
@@ -16,7 +16,7 @@
 
 namespace optiling {
 static constexpr uint32_t IN_OUT_RATE_THRESHOLD = 5;
-static constexpr uint32_t INNER_DIM_THRESHOLD = 128;
+static constexpr uint32_t INNER_DIM_THRESHOLD = 512;
 static constexpr uint64_t DCACHE_SIZE = static_cast<uint64_t>(32 * 1024);
 static constexpr uint32_t MAX_INDEX_NUM = 1024;
 static constexpr int64_t DOUBLE = 2;
@@ -25,7 +25,7 @@ static constexpr uint32_t ALIGN_SIZE = 128;
 
 bool UnsortedSegmentSortSimtTiling::IsCapable() 
 {
-    if (inputOuterDim_ / outputOuterDim_ >= IN_OUT_RATE_THRESHOLD && innerDim_ * dataTypeBytes_ < INNER_DIM_THRESHOLD) {
+    if (inputOuterDim_ / outputOuterDim_ >= IN_OUT_RATE_THRESHOLD && innerDim_ < INNER_DIM_THRESHOLD) {
         return true;
     }
     return false;
