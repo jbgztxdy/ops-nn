4d87132a4585d00ff3e4a0e3691e44dc1d13ea26
wangrui_ <wangrui124@huawei.com>
2026-03-24 17:17:00 +0800

    fix: compile error with command --opkernel -u
    Co-authored-by: wangrui<wangrui124@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3100 merge master into master
    
    fix: compile error with command --opkernel -u
    
    Created-by: wangrui_
    Commit-by: wangrui
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    修复kernel ut全量执行报错
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    https://gitcode.com/cann/ops-nn/issues/1679
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!3100

--- cmake/gen_ops_info.cmake
+++ cmake/gen_ops_info.cmake
@@ -47,17 +47,19 @@ function(kernel_src_copy)
   endforeach()
 
   # common install
-  add_custom_target(
-    atvoss_src_copy
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory ${KNCPY_DST_DIR}/common
-    COMMAND
-      bash -c "cp -r ${OPBASE_SOURCE_PATH}/pkg_inc/op_common/atvoss ${KNCPY_DST_DIR}/common"
-    COMMAND
-      bash -c "cp -r ${OPBASE_SOURCE_PATH}/pkg_inc/op_common/op_kernel ${KNCPY_DST_DIR}/common"
-    VERBATIM
-  )
-  add_dependencies(${KNCPY_TARGET} atvoss_src_copy)
+  if(NOT TARGET atvoss_src_copy)
+    add_custom_target(
+      atvoss_src_copy
+      COMMAND
+        ${CMAKE_COMMAND} -E make_directory ${KNCPY_DST_DIR}/common
+      COMMAND
+        bash -c "cp -r ${OPBASE_SOURCE_PATH}/pkg_inc/op_common/atvoss ${KNCPY_DST_DIR}/common"
+      COMMAND
+        bash -c "cp -r ${OPBASE_SOURCE_PATH}/pkg_inc/op_common/op_kernel ${KNCPY_DST_DIR}/common"
+      VERBATIM
+    )
+    add_dependencies(${KNCPY_TARGET} atvoss_src_copy)
+  endif()
   if(ENABLE_PACKAGE)
     install(DIRECTORY ${OPBASE_SOURCE_PATH}/pkg_inc/op_common/atvoss DESTINATION ${IMPL_INSTALL_DIR}/common)
     install(DIRECTORY ${OPBASE_SOURCE_PATH}/pkg_inc/op_common/op_kernel DESTINATION ${IMPL_INSTALL_DIR}/common)
