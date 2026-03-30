02fc9e99d264fcadc78d23212e95fd9abcbaa754
yourealize <chenzhanxi1@huawei.com>
2026-03-30 09:15:31 +0800

    版权信息修改
    Co-authored-by: 陈展熹<chenzhanxi1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3308 merge feature/softshrink into master
    
    版权信息修改
    
    Created-by: yourealize
    Commit-by: 陈展熹
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    版权信息修改
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    关联Issue [#1810](https://gitcode.com/cann/ops-nn/issues/1810) 
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：版权信息
    
    
    
    
    See merge request: cann/ops-nn!3308

--- activation/hard_swish_grad/op_host/arch35/hard_swish_grad_tiling_arch35.cpp
+++ activation/hard_swish_grad/op_host/arch35/hard_swish_grad_tiling_arch35.cpp
@@ -6,6 +6,9 @@
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. 
  * See LICENSE in the root of the software repository for the full text of the License.
+ *
+ * NOTE: Portions of this code were AI-generated and have been
+ * technically reviewed for functional accuracy and security
  */
 
 /*!
