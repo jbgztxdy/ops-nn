79c342d7c513cb1247384a1290c9330951bf0f6b
luoyufan7 <luoyufan7@h-partners.com>
2026-04-27 16:49:39 +0800

    modify activation op ut name
    Co-authored-by: luoyufan7<luoyufan7@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4286 merge ut into master
    
    modify activation op ut name
    
    Created-by: luoyufan7
    Commit-by: luoyufan7
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    See merge request: cann/ops-nn!4286

--- activation/celu_v2/tests/ut/op_host/op_api/test_aclnn_celu.cpp
+++ activation/celu_v2/tests/ut/op_host/op_api/test_aclnn_celu.cpp
@@ -17,7 +17,7 @@
 #include <complex>
 #include "gtest/gtest.h"
 
-#include "../../../op_host/op_api/aclnn_celu.h"
+#include "../../../../op_host/op_api/aclnn_celu.h"
 
 #include "op_api_ut_common/tensor_desc.h"
 #include "op_api_ut_common/scalar_desc.h"
