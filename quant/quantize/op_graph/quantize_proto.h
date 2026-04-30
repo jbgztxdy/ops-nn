4e7e06aa8ef2128b18516a09a0caba3d833d2380
cann-robot <songchangxia@cann.team>
2026-04-30 10:54:32 +0800

    删除quantize中多余注册原型
    Co-authored-by: ywx1080114<yuanbin22@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4407 merge QuantizeProto into master
    
    删除quantize中多余注册原型
    
    Created-by: yuanbin_22
    Commit-by: ywx1080114
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    删除quantize中多余注册原型
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    https://gitcode.com/cann/ops-nn/issues/2424
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    See merge request: cann/ops-nn!4407

--- quant/quantize/op_graph/quantize_proto.h
+++ quant/quantize/op_graph/quantize_proto.h
@@ -19,45 +19,6 @@
 namespace ge {
 
 /**
-* @brief Dequantizes the input tensor into a float tensor.
-* [min_range, max_range] are float32 tensors that specify the range
-* for "y".
-* The "mode" attribute controls exactly which calculations are used to convert
-* the float values to their quantized equivalents.
-* @par Inputs:
-* @li x: A Tensor. Must be one of the following types: qint8, quint8, qint32, quint16, qint16.
-* Shape suport 1D ~ 8D. The format support ND or NC1HWC0.
-* @li min_range: A Tensor of type float32.
-* Specifies the minimum scalar value possibly produced for the input. Shape suport 1D ~ 8D.
-* The format support ND or NC1HWC0. Has the same format as "x".
-* @li max_range: A Tensor of type float32.
-* Specifies the maximum scalar value possibly produced for the input. The format support ND or NC1HWC0.
-* Shape suport 1D ~ 8D. "max_range" has the same shape as "min_range". Has the same format as "x". \n
-
-* @par Attributes:
-* mode: An optional string from: "MIN_COMBINED", "MIN_FIRST", and "SCALED".
-* Defaults to "MIN_COMBINED" . \n
-
-* @par Outputs:
-* y: A dictionary of type float32. The format support ND or NC1HWC0.
-* "y" has the same shape and format as "x". \n
-
-* @attention Constraints:
-* @li "min_range" and "max_range" have the same shapes.
-* @li "x" and "y" have the same shapes. \n
-
-* @par Third-party framework compatibility
-* Compatible with the TensorFlow operator Dequantize.
-*/
-REG_OP(Dequantize)
-    .INPUT(x, TensorType(DT_QINT8, DT_QUINT8, DT_QINT32, DT_QINT16, DT_QUINT16))
-    .INPUT(min_range, TensorType{DT_FLOAT})
-    .INPUT(max_range, TensorType{DT_FLOAT})
-    .OUTPUT(y, TensorType({DT_FLOAT}))
-    .ATTR(mode, String, "MIN_COMBINED")
-    .OP_END_FACTORY_REG(Dequantize)
-
-/**
 * @brief Quantizes the input, scales/zero_points support broadcasting operations. \n
 *
 * @par Inputs:
