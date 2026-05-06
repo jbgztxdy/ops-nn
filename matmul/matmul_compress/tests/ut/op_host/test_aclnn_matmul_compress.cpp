c9b6a156fbe0a786c88a9b5ed1568501acf65932
zhengyuhao3 <zhengyuhao3@huawei.com>
2026-05-06 17:46:18 +0800

    matmul_compress算子缺失op_api_ut，补充api ut文件
    Co-authored-by: z00939187<zhengyuhao3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4520 merge master-compressUT into master
    
    matmul_compress算子缺失op_api_ut，补充api ut文件
    
    Created-by: zhengyuhao3
    Commit-by: z00939187
    Merged-by: cann-robot
    Description: ## 描述
    补充matmul_compress算子api UT
    
    ## 关联的Issue
    ISSUE2113
    https://gitcode.com/cann/ops-nn/issues/2113
    
    ## 测试
    ![image.png](https://raw.gitcode.com/user-images/assets/7665709/a3abb020-13c7-4131-b307-961f472e8b13/image.png 'image.png')
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：UT补充
    
    
    See merge request: cann/ops-nn!4520

---
+++ matmul/matmul_compress/tests/ut/op_host/test_aclnn_matmul_compress.cpp
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+#include <vector>
+#include <array>
+#include <float.h>
+#include "gtest/gtest.h"
+
+#include "../../../op_host/op_api/aclnn_matmul_compress.h"
+#include "op_api/op_api_def.h"
+
+#include "op_api_ut_common/tensor_desc.h"
+#include "op_api_ut_common/scalar_desc.h"
+#include "op_api_ut_common/op_api_ut.h"
+
+using namespace std;
+using namespace op;
+
+
+class l2_matmul_compress_test : public testing::Test {
+protected:
+    static void SetUpTestCase()
+    {
+        cout << "matmul_compress_test SetUp" << endl;
+    }
+
+    static void TearDownTestCase()
+    {
+        cout << "matmul_compress_test TearDown" << endl;
+    }
+
+    static void MatMulCompressTest(
+        TensorDesc x_desc, TensorDesc weight_desc, TensorDesc bias_desc, TensorDesc compressIndex_desc, 
+        TensorDesc out_desc, aclnnStatus expect_status)
+    {
+        auto ut = OP_API_UT(aclnnMatmulCompress, INPUT(x_desc, weight_desc, bias_desc, compressIndex_desc), OUTPUT(out_desc));
+        uint64_t workspaceSize = 0;
+        aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+        EXPECT_EQ(getWorkspaceResult, expect_status);
+    }
+};
+
+TEST_F(l2_matmul_compress_test, test_fp16)
+{
+    // 使用**Desc描述host api输入输出
+    TensorDesc x_desc = TensorDesc({1024, 1024}, ACL_FLOAT16, ACL_FORMAT_ND);
+    TensorDesc weight_desc = TensorDesc({1024, 1024}, ACL_FLOAT16, ACL_FORMAT_ND);
+    TensorDesc bias_desc = TensorDesc({1024}, ACL_FLOAT, ACL_FORMAT_ND);
+    TensorDesc compressIndex_desc = TensorDesc({1024}, ACL_INT8, ACL_FORMAT_ND);
+    TensorDesc out_desc = TensorDesc({1024, 1024}, ACL_FLOAT16, ACL_FORMAT_ND);
+    MatMulCompressTest(x_desc, weight_desc, bias_desc, compressIndex_desc, out_desc, ACL_SUCCESS);
+}
