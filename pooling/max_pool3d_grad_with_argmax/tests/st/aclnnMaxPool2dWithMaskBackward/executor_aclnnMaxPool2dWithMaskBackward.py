6685adcea05da8449d21a45a10b0a809c6441c98
Christina_YU <xiangzhiping1@h-partners.com>
2026-05-25 16:51:14 +0800

    新增了MaxPool2DWithMaskBackward 算子的atk用例和excutor
    Co-authored-by: Christina_YU<xiangzhiping1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5199 merge local into master
    
    新增了MaxPool2DWithMaskBackward 算子的atk用例和excutor
    
    Created-by: Christina_YU
    Commit-by: Christina_YU
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    新增了MaxPool2DWithMaskBackward 算子的atk用例和excutor
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    Issue [#2855](https://gitcode.com/cann/ops-nn/issues/2855) 
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    全部atk通过本地测试
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：新增了MaxPool2DWithMaskBackward 算子的atk用例和excutor
    
    
    See merge request: cann/ops-nn!5199

---
+++ pooling/max_pool3d_grad_with_argmax/tests/st/aclnnMaxPool2dWithMaskBackward/executor_aclnnMaxPool2dWithMaskBackward.py
@@ -0,0 +1,58 @@
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved. 
+import torch
+from atk.configs.dataset_config import InputDataset
+from atk.tasks.api_execute import register
+from atk.tasks.api_execute.base_api import BaseApi
+from atk.tasks.backends.lib_interface.acl_wrapper import AclFormat
+import numpy as np
+import torch_npu
+import math
+
+@register("ascend_method_torch_nn_MaxPool2dWithMaskBackward")
+class MethodTorchNnMaxPool2DWithMaskBackwardApi(BaseApi):
+    def __call__(self, input_data: InputDataset, with_output: bool = False):
+        gradIn = None
+        if self.device == 'cpu':
+            gradOut = input_data.kwargs["gradOutput"]
+            x = input_data.kwargs["self"]
+            kernelSize = input_data.kwargs["kernelSize"]
+            stride = input_data.kwargs["stride"]
+            padding = input_data.kwargs["padding"]
+            dilation = input_data.kwargs["dilation"]
+            ceilMode = input_data.kwargs["ceilMode"]
+            m = torch.nn.MaxPool2d(kernel_size=kernelSize, stride=stride, padding=padding,
+                                   dilation=dilation, return_indices=True, ceil_mode=ceilMode)
+            ori_type = x.dtype
+            x_float = x.to(torch.float)
+            gradOut_float = gradOut.to(torch.float)
+            out, indices = m(x_float)
+            
+            gradIn = torch.ops.aten.max_pool2d_with_indices_backward(gradOut_float, x_float, kernelSize, stride,
+                                                                     padding, dilation, ceilMode, indices.to(torch.int64))
+            return gradIn.to(ori_type)
+        return gradIn
+
+    def init_by_input_data(self, input_data: InputDataset, with_output: bool = False):
+        if self.device == 'pyaclnn':
+            x = input_data.kwargs["self"]
+            kernelSize = input_data.kwargs["kernelSize"]
+            stride = input_data.kwargs["stride"]
+            padding = input_data.kwargs["padding"]
+            dilation = input_data.kwargs["dilation"]
+            ceilMode = input_data.kwargs["ceilMode"]
+            x_cpu = x.cpu()
+            m = torch.nn.MaxPool2d(kernel_size=kernelSize, stride=stride, padding=padding, dilation=dilation,
+                                        return_indices=True, ceil_mode=ceilMode)
+            out_cpu, indices_cpu = m(x_cpu.float())
+            n = indices_cpu.shape[0]
+            c = indices_cpu.shape[1]
+            ho = indices_cpu.shape[2]
+            wo = indices_cpu.shape[3]
+            kh = kernelSize[0]
+            kw = kernelSize[1]
+            indices_npu_shape = [n, c, kh * kw, (math.ceil(ho * wo / 16) + 1) * 32]
+            pad_size = n * c * kh * kw * (math.ceil(ho * wo / 16) + 1) * 8 - indices_cpu.numel()
+            flatten_indices_cpu = indices_cpu.to(torch.int32).flatten()
+            zero_padding = torch.zeros(int(pad_size), dtype=torch.int32)
+            indices_npu_cat = torch.cat((flatten_indices_cpu, zero_padding)).view(torch.int8).reshape(indices_npu_shape).npu()
+            input_data.kwargs['indices'] = indices_npu_cat
