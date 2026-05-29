0edc59e8504edb03cd4bc41ccf1cde16638e826b
LSYlsy0214 <liushiyu25@h-partners.com>
2026-05-29 14:51:07 +0800

    foereach目录codecheck整改
    Co-authored-by: LSYlsy0214<liushiyu25@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5361 merge master into master
    
    foereach目录codecheck整改
    
    Created-by: LSYlsy0214
    Commit-by: LSYlsy0214
    Merged-by: cann-robot
    Description: ## 描述
    
    add UTF-8 encoding declaration to foreach tests/st python files
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-nn/issues/2952
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    See merge request: cann/ops-nn!5361

--- foreach/foreach_sinh/tests/st/aclnnForeachSinh/executor_aclnnForeachSinh.py
+++ foreach/foreach_sinh/tests/st/aclnnForeachSinh/executor_aclnnForeachSinh.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
 # Copyright (c) 2025 Huawei Technologies Co., Ltd.
 # This program is free software, you can redistribute it and/or modify it under the terms and conditions of
--- foreach/foreach_zero_inplace/tests/st/aclnnForeachZeroInplace/executor_aclnnForeachZeroInplace.py
+++ foreach/foreach_zero_inplace/tests/st/aclnnForeachZeroInplace/executor_aclnnForeachZeroInplace.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
 # This program is free software, you can redistribute it and/or modify.
 # Copyright (c) 2025 Huawei Technologies Co., Ltd.
