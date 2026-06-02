66eb8f5f5784483778558c3084c3c534486ace83
cann-robot <cann@cann.team>
2026-06-02 09:19:34 +0800

    gelu_quant补充aclnn的example用例
    Co-authored-by: yuanbin_22<yuanbin22@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5512 merge AddGeluQuantExample into master
    
    gelu_quant补充aclnn的example用例
    
    Created-by: yuanbin_22
    Commit-by: yuanbin_22
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    gelu_quant补充aclnn的example用例
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    https://gitcode.com/cann/ops-nn/issues/2997
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    See merge request: cann/ops-nn!5512

---
+++ activation/gelu_quant/examples/arch35/test_aclnn_gelu_quant.cpp
@@ -0,0 +1,179 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include "acl/acl.h"
+#include "aclnnop/aclnn_gelu_quant.h"
+
+#define CHECK_RET(cond, return_expr) \
+    do {                             \
+        if (!(cond)) {               \
+            return_expr;             \
+        }                            \
+    } while (0)
+
+#define LOG_PRINT(message, ...)         \
+    do {                                \
+        printf(message, ##__VA_ARGS__); \
+    } while (0)
+
+int64_t GetShapeSize(const std::vector<int64_t>& shape)
+{
+    int64_t shapeSize = 1;
+    for (auto i : shape) {
+        shapeSize *= i;
+    }
+    return shapeSize;
+}
+
+int Init(int32_t deviceId, aclrtStream* stream)
+{
+    auto ret = aclInit(nullptr);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclInit failed. ERROR: %d\n", ret); return ret);
+    ret = aclrtSetDevice(deviceId);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtSetDevice failed. ERROR: %d\n", ret); return ret);
+    ret = aclrtCreateStream(stream);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtCreateStream failed. ERROR: %d\n", ret); return ret);
+    return 0;
+}
+
+int CreateAclTensorHalf(
+    const std::vector<uint16_t>& hostData, const std::vector<int64_t>& shape, void** deviceAddr, aclTensor** tensor)
+{
+    auto size = GetShapeSize(shape) * sizeof(uint16_t);
+    auto ret = aclrtMalloc(deviceAddr, size, ACL_MEM_MALLOC_HUGE_FIRST);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtMalloc failed. ERROR: %d\n", ret); return ret);
+    ret = aclrtMemcpy(*deviceAddr, size, hostData.data(), size, ACL_MEMCPY_HOST_TO_DEVICE);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtMemcpy failed. ERROR: %d\n", ret); return ret);
+
+    std::vector<int64_t> strides(shape.size(), 1);
+    for (int64_t i = shape.size() - 2; i >= 0; i--) {
+        strides[i] = shape[i + 1] * strides[i + 1];
+    }
+
+    *tensor = aclCreateTensor(
+        shape.data(), shape.size(), aclDataType::ACL_FLOAT16, strides.data(), 0, aclFormat::ACL_FORMAT_ND, shape.data(),
+        shape.size(), *deviceAddr);
+    return 0;
+}
+
+int CreateAclTensorInt8(
+    const std::vector<int8_t>& hostData, const std::vector<int64_t>& shape, void** deviceAddr, aclTensor** tensor)
+{
+    auto size = GetShapeSize(shape) * sizeof(int8_t);
+    auto ret = aclrtMalloc(deviceAddr, size, ACL_MEM_MALLOC_HUGE_FIRST);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtMalloc failed. ERROR: %d\n", ret); return ret);
+    ret = aclrtMemcpy(*deviceAddr, size, hostData.data(), size, ACL_MEMCPY_HOST_TO_DEVICE);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtMemcpy failed. ERROR: %d\n", ret); return ret);
+
+    std::vector<int64_t> strides(shape.size(), 1);
+    for (int64_t i = shape.size() - 2; i >= 0; i--) {
+        strides[i] = shape[i + 1] * strides[i + 1];
+    }
+
+    *tensor = aclCreateTensor(
+        shape.data(), shape.size(), aclDataType::ACL_INT8, strides.data(), 0, aclFormat::ACL_FORMAT_ND, shape.data(),
+        shape.size(), *deviceAddr);
+    return 0;
+}
+
+int main()
+{
+    int32_t deviceId = 0;
+    aclrtStream stream;
+    auto ret = Init(deviceId, &stream);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("Init acl failed. ERROR: %d\n", ret); return ret);
+
+    std::vector<int64_t> xShape = {4, 2};
+    std::vector<int64_t> inputScaleShape = {1};
+    std::vector<int64_t> inputOffsetShape = {1};
+    std::vector<int64_t> yOutShape = {4, 2};
+
+    void* xDeviceAddr = nullptr;
+    void* inputScaleDeviceAddr = nullptr;
+    void* inputOffsetDeviceAddr = nullptr;
+    void* yOutDeviceAddr = nullptr;
+
+    aclTensor* x = nullptr;
+    aclTensor* inputScale = nullptr;
+    aclTensor* inputOffset = nullptr;
+    aclTensor* yOut = nullptr;
+    aclTensor* outScale = nullptr;
+
+    std::vector<uint16_t> xHostData = {15360, 16384, 17920, 46080, 51200, 51200, 48640, 0};
+    std::vector<uint16_t> inputScaleHostData = {15360};
+    std::vector<uint16_t> inputOffsetHostData = {0};
+    std::vector<int8_t> yOutHostData(8, 0);
+
+    const char* approximate = "none";
+    const char* quantMode = "static";
+    const char* roundMode = "rint";
+    int64_t dstType = 2;
+
+    ret = CreateAclTensorHalf(xHostData, xShape, &xDeviceAddr, &x);
+    std::unique_ptr<aclTensor, aclnnStatus (*)(const aclTensor*)> xTensorPtr(x, aclDestroyTensor);
+    std::unique_ptr<void, aclError (*)(void*)> xDeviceAddrPtr(xDeviceAddr, aclrtFree);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+
+    ret = CreateAclTensorHalf(inputScaleHostData, inputScaleShape, &inputScaleDeviceAddr, &inputScale);
+    std::unique_ptr<aclTensor, aclnnStatus (*)(const aclTensor*)> inputScaleTensorPtr(inputScale, aclDestroyTensor);
+    std::unique_ptr<void, aclError (*)(void*)> inputScaleDeviceAddrPtr(inputScaleDeviceAddr, aclrtFree);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+
+    ret = CreateAclTensorHalf(inputOffsetHostData, inputOffsetShape, &inputOffsetDeviceAddr, &inputOffset);
+    std::unique_ptr<aclTensor, aclnnStatus (*)(const aclTensor*)> inputOffsetTensorPtr(inputOffset, aclDestroyTensor);
+    std::unique_ptr<void, aclError (*)(void*)> inputOffsetDeviceAddrPtr(inputOffsetDeviceAddr, aclrtFree);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+
+    ret = CreateAclTensorInt8(yOutHostData, yOutShape, &yOutDeviceAddr, &yOut);
+    std::unique_ptr<aclTensor, aclnnStatus (*)(const aclTensor*)> yOutTensorPtr(yOut, aclDestroyTensor);
+    std::unique_ptr<void, aclError (*)(void*)> yOutDeviceAddrPtr(yOutDeviceAddr, aclrtFree);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+
+    uint64_t workspaceSize = 0;
+    aclOpExecutor* executor = nullptr;
+    std::unique_ptr<void, aclError (*)(void*)> workspaceAddrPtr(nullptr, aclrtFree);
+
+    ret = aclnnGeluQuantGetWorkspaceSize(
+        x, inputScale, inputOffset, approximate, quantMode, roundMode, dstType, yOut, outScale, &workspaceSize,
+        &executor);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclnnGeluQuantGetWorkspaceSize failed. ERROR: %d\n", ret); return ret);
+
+    void* workspaceAddr = nullptr;
+    if (workspaceSize > 0UL) {
+        ret = aclrtMalloc(&workspaceAddr, workspaceSize, ACL_MEM_MALLOC_HUGE_FIRST);
+        CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("allocate workspace failed. ERROR: %d\n", ret); return ret);
+        workspaceAddrPtr.reset(workspaceAddr);
+    }
+
+    ret = aclnnGeluQuant(workspaceAddr, workspaceSize, executor, stream);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclnnGeluQuant failed. ERROR: %d\n", ret); return ret);
+
+    ret = aclrtSynchronizeStream(stream);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtSynchronizeStream failed. ERROR: %d\n", ret); return ret);
+
+    auto size = GetShapeSize(yOutShape);
+    std::vector<int8_t> yOutData(size, 0);
+    ret = aclrtMemcpy(
+        yOutData.data(), yOutData.size() * sizeof(int8_t), yOutDeviceAddr, size * sizeof(int8_t),
+        ACL_MEMCPY_DEVICE_TO_HOST);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("copy yOut from device to host failed. ERROR: %d\n", ret); return ret);
+
+    for (int64_t i = 0; i < size; i++) {
+        LOG_PRINT("y[%ld] is: %d\n", i, yOutData[i]);
+    }
+
+    aclrtDestroyStream(stream);
+    aclrtResetDevice(deviceId);
+    aclFinalize();
+    return 0;
+}
