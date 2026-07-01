f37b47423da5fbe86b763eaa750faee198112363
AlbertYoung192 <yangtao363@huawei.com>
2026-07-01 11:43:54 +0800

    修复bmm基础模板及广播场景在重复MakeTensor时单次设置的L2Cache会失效，引发整网性能劣化
    Co-authored-by: y00943198<yangtao363@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !6738 merge master into master
    
    修复bmm基础模板及广播场景在重复MakeTensor时单次设置的L2Cache会失效，引发整网性能劣化
    
    Created-by: AlbertYoung192
    Commit-by: y00943198
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    修复bmm基础模板及广播场景在重复MakeTensor时单次设置的L2Cache会失效，引发整网性能劣化，改动点在ops-tensor仓，nn仓仅变更cmake的commitID
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    https://gitcode.com/cann/ops-nn/issues/3673
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    本地自验证、二级冒烟
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [ ] AI辅助编写
    
    See merge request: cann/ops-nn!6738

--- cmake/third_party/ops-tensor.cmake
+++ cmake/third_party/ops-tensor.cmake
@@ -7,7 +7,7 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 # ----------------------------------------------------------------------------
-set(OPTENSOR_TAG_ID 91e940bdf9fa142e8c457b45c683aed20cc4fbaf)
+set(OPTENSOR_TAG_ID 85220bdb3d1b5c2d256c4e6080633714af3d5a04)
 
 if(EXISTS "${PROJECT_SOURCE_DIR}/../ops-tensor")
   get_filename_component(OPTENSOR_SOURCE_PATH
