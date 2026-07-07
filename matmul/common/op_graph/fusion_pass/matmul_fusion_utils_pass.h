05e3c332ac18da24b17cd757b309646d26ed2968
jgx12 <jingaoxiang@huawei.com>
2026-07-07 10:26:54 +0800

    MatMulToMatmulV3FusionPass图融合
    Co-authored-by: jgx12<jingaoxiang@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !7097 merge matmultomatmulv3 into master
    
    MatMulToMatmulV3FusionPass图融合
    
    Created-by: jgx12
    Commit-by: jgx12
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    图融合开源，将MatMulToMatmulV3FusionPass图融合迁移到ops-nn仓，保持原图融合逻辑，优先级不变，使用es框架。
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    https://gitcode.com/cann/ops-nn/issues/3804
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    [2026/07/03 03:14:46] [INFO]-run_single.py:1946 - BatchMatMulV2,BatchMatMulV2_BatchMatMulV2_fp32_fp32_bf16_ND_ND_ND_stream_k_0623_fuzz_noalign_0046,1783048486,100.0,Pass,Pass,NA,NA,None,0.0,"no_bound","","0.0","[[11, 8, 3], [11, 3, 8176], [1, 8176], []]","['bf16', 'bf16', 'fp32', 'int8']","['ND', 'ND', 'ND', 'ND']","[[3.35, 3.75], [3.35, 3.75], [3.35, 3.75], [3.35, 3.75]]","{'adj_x1': False, 'adj_x2': False, 'allow_hf32': 0, 'dynamic_shape': [[-1, -1, -1], [-1, -1, -1], [-1]], 'fusion_name': 'BatchMatMulToBatchMatmulV3FusionPass'}","[[11, 8, 8176]]","['bf16']","None","[]","N/A","None",None,"None","None",None,"",,"","8.437",8.437,8.437,0.0,,,"",None,BatchMatMulV3,AI_CORE
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    无
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [x] AI辅助编写
    
    See merge request: cann/ops-nn!7097

---
+++ matmul/common/op_graph/fusion_pass/matmul_fusion_utils_pass.h
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+#ifndef NN_MATMUL_FUSION_UTILS_PASS_H
+#define NN_MATMUL_FUSION_UTILS_PASS_H
+
+#include <string>
+#include <type_traits>
+#include <vector>
+
+#include "graph/gnode.h"
+#include "ge/es_graph_builder.h"
+#include "ge/fusion/pass/pattern_fusion_pass.h"
+#include "platform/platform_info.h"
+
+#define FUSION_PASS_CHECK(condition, log_func, return_expr)                                                            \
+    static_assert(std::is_same<bool, std::decay<decltype(condition)>::type>::value, "condition should be bool");       \
+    do {                                                                                                               \
+        if (condition) {                                                                                               \
+            log_func;                                                                                                  \
+            return_expr;                                                                                               \
+        }                                                                                                              \
+    } while (0)
+
+namespace ops {
+
+constexpr int64_t kX1InputIdx = 0;
+constexpr int64_t kX2InputIdx = 1;
+constexpr int64_t kBiasInputIdx = 2;
+constexpr int64_t kOffsetWInputIdx = 3;
+constexpr int64_t kCaptureTensorIdx = 0;
+constexpr char kOpTypeMatMul[] = "MatMul";
+constexpr char kOpTypeMatMulV2[] = "MatMulV2";
+constexpr char kOpTypeBatchMatMul[] = "BatchMatMul";
+constexpr char kOpTypeBatchMatMulV2[] = "BatchMatMulV2";
+
+bool IsSupportL12BtBf16(const fe::PlatformInfo& platformInfo);
+
+bool CopyOtherAttrs(const ge::GNode& matchedNode, ge::GNode& v3Node, const std::string& passName);
+
+ge::es::EsTensorHolder CreateMatMulLikeNode(ge::es::EsGraphBuilder& graphBuilder, const char* opType,
+                                            const ge::es::EsTensorHolder& x1, const ge::es::EsTensorHolder& x2,
+                                            const ge::es::EsTensorHolder& bias, const ge::es::EsTensorHolder& offsetW);
+
+std::vector<ge::fusion::PatternUniqPtr> BuildMatMulPatterns(const std::string& prefix);
+std::vector<ge::fusion::PatternUniqPtr> BuildMatMulV2Patterns(const std::string& prefix);
+std::vector<ge::fusion::PatternUniqPtr> BuildBatchMatMulPatterns(const std::string& prefix);
+std::vector<ge::fusion::PatternUniqPtr> BuildBatchMatMulV2Patterns(const std::string& prefix);
+
+} // namespace ops
+
+#endif // NN_MATMUL_FUSION_UTILS_PASS_H
